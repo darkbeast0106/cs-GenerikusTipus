c54f6f462964bad7850562d8dde3ca68a51404d0
Budaházi Bence <budahazi.bence@petrik.hu>
2022-11-22 18:09:26 +0100

    Add project files.

---
+++ GenerikusTipus/Program.cs
@@ -0,0 +1,25 @@
+﻿using GenerikusTipus;
+
+BuszAtszallas("102.txt", "105.txt");
+    
+void BuszAtszallas(string egyikFajlNev, string masikFajlNev)
+{
+    Set<string> egyikBuszMegalloi = HalmazFajlbol(egyikFajlNev);
+    Set<string> masikBuszMegalloi = HalmazFajlbol(masikFajlNev);
+    Set<string> kozosMegallok = egyikBuszMegalloi.Intersection(masikBuszMegalloi);
+    Console.WriteLine("Az egyik buszról a másikra {0} helyen lehet átszállni. Ezek a megállók:" +
+        "\n{1}", kozosMegallok.Size, kozosMegallok);
+}
+
+Set<string> HalmazFajlbol(string fajlNev)
+{
+    Set<string> halmaz = new Set<string>();
+    using (var reader = new StreamReader(fajlNev))
+    {
+        while (!reader.EndOfStream)
+        {
+            halmaz.Add(reader.ReadLine());
+        }
+    }
+    return halmaz;
+}
---
+++ GenerikusTipus/Set.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace GenerikusTipus
+{
+    public class Set<T>
+    {
+        private List<T> elements;
+
+        public Set()
+        {
+            this.elements = new List<T>();
+        }
+
+        public bool Add(T element)
+        {
+            bool successful = false;
+            if (!this.elements.Contains(element))
+            {
+                this.elements.Add(element);
+                successful = true;
+            }
+            return false;
+        }
+
+        public bool Remove(T element)
+        {
+            return this.elements.Remove(element);
+        }
+
+        public int Size
+        {
+            get => this.elements.Count;
+        }
+
+        public void Clear()
+        {
+            this.elements.Clear();
+        }
+
+        public bool Contains(T element)
+        {
+            return this.elements.Contains(element);
+        }
+
+        public bool IsEmpty()
+        {
+            return this.elements.Count == 0;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is Set<T>)) return false;
+            Set<T> other = (Set<T>)obj;
+
+            if (other.Size != this.Size) return false;
+            foreach (T element in this.elements)
+            {
+                if (!other.Contains(element))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsPartOf(Set<T> other)
+        {
+            if (this.Size > other.Size) return false;
+
+            foreach (T element in this.elements)
+            {
+                if (!other.Contains(element))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public Set<T> Unio(Set<T> other)
+        {
+            Set<T> unio = new Set<T>();
+            foreach (T element in this.elements)
+            {
+                unio.Add(element);
+            }
+            foreach (T element in other.elements)
+            {
+                unio.Add(element);
+            }
+            return unio;
+        }
+
+        public Set<T> Intersection(Set<T> other)
+        {
+            Set<T> intersection = new Set<T>();
+            foreach(T element in this.elements)
+            {
+                if (other.Contains(element))
+                {
+                    intersection.Add(element);
+                }
+            }
+            return intersection;
+        }
+
+        public Set<T> Different(Set<T> other)
+        {
+            Set<T> different = new Set<T>();
+            foreach (T element in this.elements)
+            {
+                if (!other.Contains(element))
+                {
+                    different.Add(element);
+                }
+            }
+            return different;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (T element in this.elements)
+            {
+                stringBuilder.AppendLine(element.ToString());
+            }
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
